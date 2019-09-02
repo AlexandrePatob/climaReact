2092d5a5e58f15b028f21c75082bdf4546e67355
AlexandrePatob <borges_2.0@hotmail.com>
2019-09-02 20:01:12 -0300

    Add Temp Max / Temp Min + Descriçoes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 class App extends Component {
 	state = {
-		cidade: '',
+    cidade: '',
+    city:'',
 		clima: '',
 		temperatura: '',
 		temperatura_max: '',
@@ -15,6 +16,7 @@ class App extends Component {
 			`https://testeapiclima.herokuapp.com/${cidade}`
 		);
 		this.setState({
+      city: response.data.name,
 			clima: response.data.weather[0].description,
 			temperatura: response.data.main.temp,
 			temperatura_min: response.data.main.temp_min,
@@ -51,7 +53,7 @@ class App extends Component {
 					</button>
 				</div>
 				<div>
-					<h1>Cidade:{this.state.cidade}</h1>
+					<h1>Cidade:{this.state.city}</h1>
 					<h1>Clima:{this.state.clima}</h1>
 					<h1>Temperatura:{this.state.temperatura}</h1>
 					<h1>Temp Minima:{this.state.temperatura_min}</h1>
