42fc9880cd101d30301ecc519096add53a961c49
AlexandrePatob <borges_2.0@hotmail.com>
2019-09-02 19:52:54 -0300

    Add Temp Max / Temp Min + Descriçoes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
-  state = { cidade: "", clima: "" ,temperatura:""};
+	state = {
+		cidade: '',
+		clima: '',
+		temperatura: '',
+		temperatura_max: '',
+		temperatura_min: ''
+	};
 
-  chamarApiDoPato = async cidade => {
-    const response = await axios.get(
-      `https://testeapiclima.herokuapp.com/${cidade}`
-    );
-    this.setState({ clima: response.data.weather[0].description, temperatura: response.data.main.temp});
-  };
+	chamarApiDoPato = async cidade => {
+		const response = await axios.get(
+			`https://testeapiclima.herokuapp.com/${cidade}`
+		);
+		this.setState({
+			clima: response.data.weather[0].description,
+			temperatura: response.data.main.temp,
+			temperatura_min: response.data.main.temp_min,
+			temperatura_max: response.data.main.temp_max
+		});
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <input
-            type="text"
-            value={this.state.cidade}
-            onChange={e => this.setState({ cidade: e.target.value })}
-          />
-          <button
-            onClick={async () => await this.chamarApiDoPato(this.state.cidade)}
-          >
-            Ver clima
-          </button>
-          <button
-            onClick={() => {
-              this.setState({ cidade: "", clima: "" , temperatura: ""});
-            }}
-          >
-            Limpar
-          </button>
-        </div>
-        <div>
-          <h1>{this.state.clima}</h1>
-          <h1>{this.state.temperatura}</h1>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<div>
+					<input
+						type='text'
+						value={this.state.cidade}
+						onChange={e => this.setState({ cidade: e.target.value })}
+					/>
+					<button
+						onClick={async () => await this.chamarApiDoPato(this.state.cidade)}
+					>
+						Ver clima
+					</button>
+					<button
+						onClick={() => {
+							this.setState({
+								cidade: '',
+								clima: '',
+								temperatura: '',
+								temperatura_min: '',
+								temperatura_max: ''
+							});
+						}}
+					>
+						Limpar
+					</button>
+				</div>
+				<div>
+					<h1>Cidade:{this.state.cidade}</h1>
+					<h1>Clima:{this.state.clima}</h1>
+					<h1>Temperatura:{this.state.temperatura}</h1>
+					<h1>Temp Minima:{this.state.temperatura_min}</h1>
+					<h1>Temp Maxima:{this.state.temperatura_max}</h1>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
